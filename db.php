e4b03ad862661436d2523853e57f0e95b15b0d17
chandanswain9178-rgb <chandanswain9178@gmail.com>
2025-07-21 17:05:34 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sqlXXX.epizy.com"; // Replace with your InfinityFree DB host
-$dbname = "epiz_XXXXXXX_mydb"; // Replace with your DB name
-$username = "epiz_XXXXXXX"; // Replace with your DB username
-$password = "your_db_password"; // Set in InfinityFree
+$host = "sql107.infinityfree.com"; // Replace with your InfinityFree DB host
+$dbname = "if0_39519479_db"; // Replace with your DB name
+$username = "if0_39519479"; // Replace with your DB username
+$password = "ez99J5U9wGGQ08"; // Set in InfinityFree
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
